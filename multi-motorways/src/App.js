adb2bc60b84bf6190c7bce57ce9959b7d7ba0a96
aap9002 <aap9002@gmail.com>
2023-08-18 21:59:11 +0100

    ground and basic road places on canvas

--- multi-motorways/src/App.js
+++ multi-motorways/src/App.js
@@ -2,6 +2,20 @@ import { Canvas } from "@react-three/fiber";
 
 import "./styles/styles.css";
 
+const GroundMesh = () => {
+  return <mesh position={[0, 0, -0.5]} scale={100}>
+    <planeGeometry />
+    <meshToonMaterial color="lightgreen" />
+  </mesh>
+}
+
+const RoadTile = (x,y) => {
+  return <mesh scale={0.2} rotation-z={1}> 
+      <planeGeometry args={[20,1,1]}/>
+      <meshToonMaterial color="darkgray" />
+    </mesh>
+}
+
 function App() {
   return (
     <div className="Fill">
@@ -9,10 +23,9 @@ function App() {
         <ambientLight intensity={1} />
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
           <pointLight position={[-10, -10, -10]} />
-        <mesh> 
-          <planeGeometry/>
-          <meshToonMaterial color="white" />
-        </mesh>
+          
+          <GroundMesh />
+          <RoadTile x={0} y={0}></RoadTile>
       </Canvas>
     </div>
   );
